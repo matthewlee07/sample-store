2a45e010c23e48ff9c48195810ab2a6cc21bb365
Matt Lee <matthewlee07@gmail.com>
2019-09-30 18:08:46 -0400

    sign in and sign out with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,8 +3,9 @@ import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import { Link } from "react-router-dom";
+import { auth } from "../../firebase/firebase.utils";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -14,8 +15,17 @@ const Header = () => (
         SHOP
       </Link>
       <Link className="option" to="/contact">
-        Contact
+        CONTACT
       </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="options" to="/signin">
+          SIGN IN
+        </Link>
+      )}
     </div>
   </div>
 );
