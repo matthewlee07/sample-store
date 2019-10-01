9f60adfbfce537a95142d62d50aed2ffc468f8d3
Matt Lee <matthewlee07@gmail.com>
2019-09-30 21:46:23 -0400

    some issue with firebase

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Header from "./components/header/header.component";
 class App extends React.Component {
   constructor() {
     super();
+
     this.state = {
       currentUser: null
     };
@@ -20,10 +21,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    auth.onAuthStateChanged(async user => {
-      // this.setState({ currentUser: user });
-      createUserProfileDocument(user);
-      // console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
