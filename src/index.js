85ad4e0ef61a70d501d77af6928624320958a33a
Matt Lee <matthewlee07@gmail.com>
2019-10-02 02:33:35 -0400

    state persists

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import store from "./redux/store";
-
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,9 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
 import { createStore, applyMiddleware } from "redux";
-
+import { persistStore } from "redux-persist";
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
