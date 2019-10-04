9e1ed387a5137eed006fd393f5be51d585c02508
Matt Lee <matthewlee07@gmail.com>
2019-10-03 22:15:08 -0400

    converted some to style components

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import "./collection.styles.scss";
+import {
+  CollectionPageContainer,
+  CollectionTitle,
+  CollectionItemsContainer
+} from "./collection.styles";
 
 import { connect } from "react-redux";
 import { selectCollection } from "../../redux/shop/shop.selectors";
@@ -8,14 +12,14 @@ import CollectionItem from "../../components/collection-item/collection-item.com
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
+    <CollectionPageContainer>
+      <CollectionTitle>{title}</CollectionTitle>
+      <CollectionItemsContainer>
         {items.map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </CollectionItemsContainer>
+    </CollectionPageContainer>
   );
 };
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-
+import { ShopPageContainer } from "./shop.styles";
 import { Route } from "react-router-dom";
 
 import CollectionPage from "../collection/collection.component";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
 const ShopPage = ({ match }) => (
-  <div className="shop-page">
+  <ShopPageContainer>
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
     <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
+  </ShopPageContainer>
 );
 
 export default ShopPage;
