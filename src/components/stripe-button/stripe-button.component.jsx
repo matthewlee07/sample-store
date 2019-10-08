ec46a1e5c81a2927bf9c666633d0275136ad6f41
Matt Lee <matthewlee07@gmail.com>
2019-10-08 12:44:17 -0400

    Revert "."
    This reverts commit adbe838cbb49251a167e474ba3159dbf0c7462d7.

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_aTp0tIG3B9oy4w04krnadFnK00Z0Za4jfI";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Succesful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Sample Store"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
